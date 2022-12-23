516f083975f9c3fcdfe4c55b79ad69f55d931fee
RAHMAHX1 <hafizur.rahman@abbvie.com>
2022-12-23 17:30:21 +0600

    Learning dependency injection

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 (function (){
     'use strict'
-    angular.module('myFirstApp', [])
-    .controller('myFirstController', function($scope){
-        $scope.name = "Ulala"
-        $scope.grettings = function(){
-            return "Hy look like you are in the right place. We may have fun!! Lets' Go.!!"
+    angular.module('DependencyInjectionApp', [])
+    .controller('DependencyInjectionAppController', DIController);
+    DIController.$inject = ['$scope','$filter']
+    function DIController($scope,$filter){
+        $scope.name = "";
+        $scope.upperName = "";
+        $scope.upper = function(){
+            var uPcase = $filter('uppercase');
+            $scope.upperName = uPcase($scope.name);
         };
-    });
-
+    };
 })();
