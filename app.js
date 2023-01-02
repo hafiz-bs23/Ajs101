261b2cd1899f415c9265a5184d1a801ef45de70c
RAHMAHX1 <hafizur.rahman@abbvie.com>
2023-01-02 17:51:17 +0600

    Custom filter implementation

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 (function (){
     'use strict'
-    angular.module('myFirstApp', [])
-    .controller('myFirstController', function($scope){
-        $scope.name = "Ulala"
-        $scope.grettings = function(){
-            return "Hy look like you are in the right place. We may have fun!! Lets' Go.!!"
-        };
-    });
+    angular.module('angularApp', [])
+    .controller('ctrl', cltrFunc)
+    .filter('revr', myCustomFilterFactory)
+    .filter('cntr',myCustomCounterFilterFactory);
 
+    cltrFunc.$inject = ['$scope','revrFilter','cntrFilter'];
+
+    function cltrFunc($scope, revrFilter){
+        $scope.firstName = "";
+        $scope.lastName = "";
+    };
+    function myCustomFilterFactory(){
+        return function (input){
+            input = input || "";
+            var newString = "";
+            for (let i = input.length - 1; i >= 0; i--) {
+                newString += input[i];
+            }
+            return newString;
+        };
+    };
+    function myCustomCounterFilterFactory(){
+        return function(input, arg){
+            input = input || "";
+            var totalCount = 0;
+            for (var i =0; i< input.length; i++){
+                totalCount += input.charCodeAt(i);
+            }
+            return arg ==="do" ? totalCount: 0;
+        }
+    }
 })();
